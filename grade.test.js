048a81ab6b3062ce1b712b4fe75698a4dccdce5b
Michael Ngugi <michaelrealz17@gmail.com>
2023-04-10 15:40:30 +0300

    UI and Unit Tests

---
+++ grade.test.js
@@ -0,0 +1,27 @@
+const { calculateTotal, median } = require('./calculations');
+const puppeteer = require("puppeteer");
+var testArray = [2, 3, 4, 5, 1, 6, 7];
+
+test ('Q1=30, Q2=60: total=100', () => {
+    expect(calculateTotal(30, 60)).toBe(100);
+});
+
+test ('median test =4', () => {
+    expect(median(testArray)).toBe(4);
+});
+
+// /*
+describe ('testing with puppeteer' , () =>{
+    beforeAll(async () => {
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
+    });
+
+    it('check title of page', async () => {
+        await page.goto("/");
+        const pageTitle = await page.title();
+        expect(pageTitle).toEqual('Student Grading System');
+        await browser.close();
+    }, 70000);
+});
+// */
